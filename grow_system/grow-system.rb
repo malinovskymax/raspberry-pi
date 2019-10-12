0037eaf8416f87d7ad8ec80138a9ccaf74186a83
Max <malinovsky.dev@gmail.com>
2019-10-12 23:24:43 +0300

    initial commit

---
+++ grow_system/grow-system.rb
@@ -0,0 +1,45 @@
+require 'rpi_gpio'
+
+LIGHT_PIN = 14
+
+SUMMER_MONTHS      = 6..8
+WINTER_LIGHT_HOURS = 7..21
+
+RPi::GPIO.set_numbering :board
+
+RPi::GPIO.setup LIGHT_PIN, as: :output
+
+def light_on
+  RPi::GPIO.set_high LIGHT_PIN
+end
+
+def light_off
+  RPi::GPIO.set_low LIGHT_PIN
+end
+
+def season(time = Time.now)
+  case time.month
+  when SUMMER_MONTHS
+    :summer
+  else
+    :winter
+  end
+end
+
+def light_hours(season)
+  Object.const_get("#{season}_LIGHT_HOURS".upcase)
+end
+
+def light_needed?(time)
+  light_hours(season(time)).include? time.hour
+end
+
+loop do
+  if light_needed?(Time.now)
+    light_on
+  else
+    light_off
+  end
+
+  sleep(1000)
+end
