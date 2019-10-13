58e47951ef4f5017fee961ed98a6a0fc57a07c64
Max <malinovsky.dev@gmail.com>
2019-10-13 10:25:09 +0300

    Add more settings

--- grow_system/grow-system.rb
+++ grow_system/grow-system.rb
@@ -1,22 +1,28 @@
-# Version 1.0.0
+# Version 1.1.0
 
 require 'rpi_gpio'
 
-LIGHT_PIN = 8
+# settings
+LIGHT_PIN      = 8
+RELAY_ON_LEVEL = :low
 
 SUMMER_MONTHS      = 6..8
 WINTER_LIGHT_HOURS = 7..21
+SUMMER_LIGHT_HOURS = [7, 8, 9, 10, 11, 12, 13, 20, 21]
+# settings end
 
 RPi::GPIO.set_numbering :board
 
 RPi::GPIO.setup LIGHT_PIN, as: :output
 
 def light_on
-  RPi::GPIO.set_high LIGHT_PIN
+  RPi::GPIO.send("set_#{RELAY_ON_LEVEL}", LIGHT_PIN)
 end
 
 def light_off
-  RPi::GPIO.set_low LIGHT_PIN
+  off_level = :high
+  off level = :low if RELAY_ON_LEVEL == :high
+  RPi::GPIO.send("set_#{off_level}", LIGHT_PIN)
 end
 
 def season(time = Time.now)
