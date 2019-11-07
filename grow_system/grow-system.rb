9abcfa7f407348f6fc238fdab3c858f97173d166
Max Malinovsky <malinovsky.dev@gmail.com>
2019-11-07 20:09:57 +0200

    Refactor code in OOP paradigm

--- grow_system/grow-system.rb
+++ grow_system/grow-system.rb
@@ -1,69 +1,11 @@
-# Version 1.2.4
-
 require 'rpi_gpio'
 
-# settings
-LIGHT_PIN      = 8
-RELAY_ON_LEVEL = :low
+require 'light_control'
 
-AUTUMN_MONTHS = 9..11
-WINTER_MONTHS = [12, 1, 2]
-SPRING_MONTHS = 3..5
-SUMMER_MONTHS = 6..8
-
-AUTUMN_LIGHT_HOURS = [6, 7, 8, 9, 10, 11, 12, 17, 18, 19, 20]
-WINTER_LIGHT_HOURS = 6..20
-SPRING_LIGHT_HOURS = [6, 7, 8, 9, 10, 11, 12, 17, 18, 19, 20]
-SUMMER_LIGHT_HOURS = [6, 7, 8, 9, 10, 20]
-# settings end
-
-RPi::GPIO.set_numbering :board
-
-RPi::GPIO.setup LIGHT_PIN, as: :output
-
-$light_is_on = false
-
-def light_on
-  return if $light_is_on
-
-  RPi::GPIO.send("set_#{RELAY_ON_LEVEL}", LIGHT_PIN)
-  $light_is_on = true
-end
-
-def light_off
-  return unless $light_is_on
-
-  off_level = :high
-  off_level = :low if RELAY_ON_LEVEL == :high
-  RPi::GPIO.send("set_#{off_level}", LIGHT_PIN)
-  $light_is_on = false
-end
-
-def season(time)
-  case time.month
-  when AUTUMN_MONTHS
-    :autumn
-  when WINTER_MONTHS
-    :winter
-  when SPRING_MONTHS
-    :spring
-  when SUMMER_MONTHS
-    :summer
-  else
-    raise "current month number: #{time.month} does not belong to any season defined in the `settings` section"
-  end
-end
-
-def light_hours(season)
-  Object.const_get("#{season}_LIGHT_HOURS".upcase)
-end
-
-def light_needed?(time = Time.now)
-  light_hours(season(time)).include? time.hour
-end
+light_controller = LightControl.new(RPi::GPIO)
 
 loop do
-  light_needed? ? light_on : light_off
+  light_controller.tick
 
   sleep(1)
 end
---
+++ grow_system/light_control.rb
@@ -0,0 +1,63 @@
+class LightControl
+  def initialize(gpio, options = {})
+    raise 'nil gpio interace' if gpio.nil?
+
+    @gpio           = gpio
+    @light_pin      = options[:light_pin]      || 8
+    @relay_on_level = options[:relay_on_level] || :low
+
+    @gpio.set_numbering(:board)
+    @gpio.setup(@light_pin, as: :output)
+
+    @light_is_on = false
+
+    @autumn_months = options[:autumn_months] || (9..11)
+    @winter_months = options[:winter_months] || [12, 1, 2]
+    @spring_months = options[:spring_months] || (3..5)
+    @summer_months = options[:summer_months] || (6..8)
+
+    @autumn_light_hours = options[:autumn_light_hours] || (6..20)
+    @winter_light_hours = options[:winter_light_hours] || (6..20)
+    @spring_light_hours = options[:spring_light_hours] || (6..20)
+    @summer_light_hours = options[:summer_light_hours] || (6..20)
+  end
+
+  def tick
+    light_needed? ? light_on : light_off
+  end
+
+  private
+
+  def light_on
+    return if @light_is_on
+
+    @gpio.send("set_#{@relay_on_level}", @light_pin)
+    @light_is_on = true
+  end
+
+  def light_off
+    return unless @light_is_on
+
+    off_level = :high
+    off_level = :low if @relay_on_level == :high
+    @gpio.send("set_#{off_level}", @light_pin)
+    @light_is_on = false
+  end
+
+  def season(month)
+    return :autumn if @autumn_months.include? month
+    return :winter if @winter_months.include? month
+    return :spring if @spring_months.include? month
+    return :summer if @summer_months.include? month
+
+    raise "month number: #{month} does not belong to any season defined in the settings.rb"
+  end
+
+  def light_hours(season)
+    instance_variable_get("@#{season}_light_hours")
+  end
+
+  def light_needed?(time = Time.now)
+    light_hours(season(time.month)).include? time.hour
+  end
+end
